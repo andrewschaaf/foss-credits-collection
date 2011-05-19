ba1e72ae7e64b299842699ff807deda91762c949
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-18 21:12:52 -0400

    fix paths via __dirname

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ var fs = require('fs');
 var licenses = {}
 
 var filenames = fs.readdirSync('licenses');
-var filename, name, m, data, coffee, js;
+var filename, name, m, coffee, js;
 for (var i = 0, len = filenames.length; i < filenames.length; i++) {
   filename = filenames[i];
   if (m = filename.match(/^([^.].*)\.coffee$/)) {
     name = m[1];
-    data = fs.readFileSync("licenses/" + filename);
-    coffee = data.toString('utf-8');
+    path = __dirname + "/licenses/" + filename;
+    coffee = fs.readFileSync(path).toString('utf-8');
     js = "(function(){ " + coffee_script.compile(coffee)  + " })();"
     eval(js);
     licenses[name] = module.exports;
